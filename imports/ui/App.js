9f296f1a8ed82cdf2deaa28b8847fc1ff03f2940
Andreas Stassivik <andreas@stassi.net>
2018-09-18 02:15:53 -0700

    client boilerplate

---
+++ imports/ui/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+export default function App() {
+  return (
+    <Fragment>
+      <CssBaseline />
+      <h1>
+        Hello world!
+      </h1>
+    </Fragment>
+  );
+}
